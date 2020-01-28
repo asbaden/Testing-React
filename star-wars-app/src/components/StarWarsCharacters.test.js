3a10a718f6a1c3099b29dfea1d78a6442429f36b
Gus Baden <gusbaden96@gamil.com>
2020-01-28 15:09:16 -0800

    finished testing

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { render, fireEvent, wait } from "@testing-library/react";
-import StarWardsCharacters from "./StarWarsCharacters";
-import { getData as mockGetData } from "../api/getData";
+import StarWarsCharacters from "./StarWarsCharacters";
+import { getData as mockGetData } from "../api/";
 
 
 jest.mock('../api')
 
+const testData = {count:80, next: "nextUrl", previous:"previousUrl", results: [{name:"gus", url: 'url'}]}
+
+
 test(`renders the  previos and next buttons`, () => {
-    const { getByText } = render(<StarWardsCharacters />);
+    mockGetData.mockResolvedValue(testData)
+    
+    const { getByText } = render(<StarWarsCharacters />);
     
     const previosButton = getByText(/previous/i);
     const nextButton = getByText(/next/i);
@@ -16,17 +21,19 @@ test(`renders the  previos and next buttons`, () => {
 
 
 
-
-
 test('api test', async () => {
-    const testData = {count:80, next: "nextUrl", previous:"previousUrl", results: [{name:"gus", url: 'url'}]}
+    
        mockGetData.mockResolvedValueOnce(testData);
        const { getByText } = render(<StarWarsCharacters />);
-       const nextButton = getByText(/next/i)
+       const nextButton = getByText(/next/i);
        fireEvent.click(nextButton);
    await wait(() => {
        expect(getByText(/gus/i)).toBeTruthy();
        expect(mockGetData).toHaveBeenCalledTimes(2);
    });
 });
+
+
+
+
 
